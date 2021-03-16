3a03d4d107fb1020a2edf011d05164dce8eaa878
Ayush Sanj <ayushsanjdev@users.noreply.github.com>
2021-03-16 16:49:17 +0530

    added the solution and closed

---
+++ boowho.js
@@ -0,0 +1,6 @@
+function booWho(bool) { //creating function first
+	if (typeof(bool) === "boolean") { //using if statements to check 
+		return true; //if bool's type is any boolean then return
+	} //else
+	return false; //false
+}
